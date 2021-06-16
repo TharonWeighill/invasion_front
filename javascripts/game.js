c421b0ed56b81a3a96c22f95ab838e84e6fc44fa
tharon <tharonweighill@gmail.com>
2021-06-16 14:49:12 -0600

    add sprites

--- javascripts/game.js
+++ javascripts/game.js
@@ -4,3 +4,18 @@ kaboom ({
     scale: 1,
     debug: true,
 })
+
+loadRoot('https://replit.com/@tharonweighill/Alien-Attack#sprites/')
+loadSprite('b', '')
+loadSprite('e', '')
+loadSprite('f', '')
+loadSprite('g', '')
+loadSprite('h', '')
+loadSprite('j', '')
+loadSprite('m', '')
+loadSprite('n', '')
+loadSprite('q', '')
+loadSprite('s', '')
+loadSprite('t', '')
+loadSprite('v', '')
+loadSprite('z', '')
