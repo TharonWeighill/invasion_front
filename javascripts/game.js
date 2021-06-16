9add77e3db437004acbd610f4729d6767c46f759
tharon <tharonweighill@gmail.com>
2021-06-16 15:01:20 -0600

    level config

--- javascripts/game.js
+++ javascripts/game.js
@@ -31,6 +31,23 @@ scene("game", ( => {
         'nnnnnnnnnnnnnnnnzzzzzzzzzzzzzzzzzzzznnnnnnnnnnnnnnzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
     ]
 
+    const levelCfg = {
+        width: 40,
+        height: 40,
+  'q': [ sprite('q'), scale(2), solid(), 'q', 'dangerous'],
+  'h': [ sprite('h'), scale(2), solid(), 'h','dangerous'],
+  'z': [ sprite('z'), scale(4), solid(1), 'z','dangerous'],
+  'j': [ sprite('j'), scale(.5), solid(1), 'j','dangerous'],
+  'n': [ sprite('n'), scale(4), solid(), 'n','dangerous'],
+  's': [ sprite('s'), scale(8), solid(), 's','dangerous'],
+  't': [ sprite('t'), scale(2), solid(), 't','dangerous'],
+  'g': [ sprite('g'), scale(6), solid(), 'g','dangerous'],
+  'f': [ sprite('f'), scale(2), solid(), 'f','dangerous'],
+  'm': [ sprite('m'), scale(2), solid(), 'm','dangerous'],
+  'b': [ sprite('b'), scale(2), solid(), 'a','dangerous'],
+  'e': [ sprite('e'), scale(3), solid(), 'e','dangerous'],
+    }
+
 })
 
 start("game")
