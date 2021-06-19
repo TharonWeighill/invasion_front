fb9d30b6079c592aa018808307892be3ff736654
tharon <tharonweighill@gmail.com>
2021-06-19 14:50:24 -0600

    add class session

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,256 +1,230 @@
-let state = {
-    player: {
-        id: null,
-        name: null
-    },
-    gameInProgress: false
-};
-
-const playerForm = document.getElementById('player-form');
-playerForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const playerNameInput = document.getElementById('player-name-input');
-    return fetch('http://localhost:3000/players', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: playerNameInput.value
-        })
-    }).then((response) => response.json())
-        .then((playerData) => Object.assign(state, {
-            id: playerData.id,
-            name: playerData.name
-        }))
-        .catch((error) => console.error(error));
-});
-
-kaboom({
-    global: true,
-
-    canvas: document.getElementById('game'),
-    scale: 2,
-    debug: true,
-    clearColor: [0, 0, 0, 1]
-});
-
-loadRoot('../sprites/');
-loadSprite('b', 'b.png');
-loadSprite('e', 'e.png');
-loadSprite('f', 'f.png');
-loadSprite('g', 'g.png');
-loadSprite('h', 'h.png');
-loadSprite('j', 'j.png');
-loadSprite('m', 'm.png');
-loadSprite('n', 'n.png');
-loadSprite('q', 'q.png');
-loadSprite('s', 's.png');
-loadSprite('t', 't.png');
-loadSprite('v', 'v.png');
-loadSprite('z', 'z.png');
-
-scene("game", () => {
-    layers(['bg', 'obj', 'ui'], 'obj');
-
-    const map = [
-        '                                                                                   ',
-        '                                                                                   ',
-        '                                                                                   ',
-        '                                                                                   ',
-        '                                                                                   ',
-        'w         j   j    j    j    j  m   mm                                             ',
-        'w   j   j   j   jttt  j   ttt                                                      ',
-        'w     j    sss   gffgffg                                                           ',
-        'wbbbbeehqhbbbbbeeeeqheeebhebehqbhbebebebebbebebeeeee                               ',
-        'nnnnnnnnnnnnnnnnzzzzzzzzzzzzzzzzzzzznnnnnnnnnnnnnnzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
-    ];
-
-    const levelCfg = {
-        width: 40,
-        height: 40,
-        'q': [sprite('q'), scale(2), solid(), 'q', 'dangerous'],
-        'h': [sprite('h'), scale(2), solid(), 'h', 'dangerous'],
-        'z': [sprite('z'), scale(4), solid(1), 'z', 'dangerous'],
-        'j': [sprite('j'), scale(.5), solid(1), 'j', 'dangerous'],
-        'n': [sprite('n'), scale(4), solid(), 'n', 'dangerous'],
-        's': [sprite('s'), scale(8), solid(), 's', 'dangerous'],
-        't': [sprite('t'), scale(2), solid(), 't', 'dangerous'],
-        'g': [sprite('g'), scale(6), solid(), 'g', 'dangerous'],
-        'f': [sprite('f'), scale(2), solid(), 'f', 'dangerous'],
-        'm': [sprite('m'), scale(2), solid(), 'm', 'dangerous'],
-        'b': [sprite('b'), scale(2), solid(), 'a', 'dangerous'],
-        'e': [sprite('e'), scale(3), solid(), 'e', 'dangerous'],
+class Session {
+    getPlayer() {
+        const body = document.getElementById('app');
+        body.innerHTML = `
+        <form id='player-form'>
+            <h1> Alien Invasion!!! </h1>
+            <label>Enter Name</label>
+            <input />
+            <button type='submit'>START GAME</button
+        </form> 
+        `;
+        const form = document.getElementById('player-form');
+        form.addEventListener('submit', this.handleFromSubmit);
     };
-
-    const gameLevel = addLevel(map, levelCfg);
-
-    const player = add([
-        sprite('v'),
-        scale(.5),
-        pos(20, 20)
-    ]);
-
-    player.collides('dangerous', () => {
-        destroy(player);
-    });
-
-    player.action(() => {
-        camPos(player.pos),
-            camScale(.55);
-    });
-
-    const TIME_LEFT = 30;
-
-    const timer = add([
-        text('0'),
-        pos(player.pos),
-        scale(.5),
-        layer('ui'),
-        {
-            time: TIME_LEFT,
-        },
-    ]);
-
-    timer.action(() => {
-        timer.time -= dt();
-        timer.text = timer.time.toFixed(2);
-        if (timer.time <= 0) {
-            go('over, score.value');
-        }
-
-    });
-
-    const score = add([
-        text('0'),
-        pos(5, 5),
-        layer('ui'),
-        {
-            value: 0,
-        }
-    ]);
-
-    const MOVE_SPEED = 100;
-
-    keyDown('left', () => {
-        player.move(-MOVE_SPEED, 0);
-    });
-    keyDown('right', () => {
-        player.move(MOVE_SPEED, 0);
-    });
-    keyDown('up', () => {
-        player.move(0, -MOVE_SPEED);
-    });
-    keyDown('down', () => {
-        player.move(0, MOVE_SPEED);
-    });
-
-    function spawnBullet(p) {
-        add([
-            rect(5, 10),
-            pos(p),
-            origin('center'),
-            color(.1, 0.9, 1),
-            'bullet'
-        ]);
+    handleFromSubmit(e) {
+        e.preventDefault();
+        console.log("yo");
     }
+}
+const session = new Session();
+session.getPlayer();
 
-    const BULLET_SPEED = 100;
-    action('bullet', (b) => {
-        b.move(0, +BULLET_SPEED);
-        if (b.pos.y < 0) {
-            destroy(b);
-        }
-    });
+// kaboom({
+//     global: true,
+
+//     canvas: document.getElementById('game'),
+//     scale: 1,
+//     debug: true,
+//     clearColor: [0, 0, 0, 1]
+// });
+
+// loadRoot('../sprites/');
+// loadSprite('b', 'b.png');
+// loadSprite('e', 'e.png');
+// loadSprite('f', 'f.png');
+// loadSprite('g', 'g.png');
+// loadSprite('h', 'h.png');
+// loadSprite('j', 'j.png');
+// loadSprite('m', 'm.png');
+// loadSprite('n', 'n.png');
+// loadSprite('q', 'q.png');
+// loadSprite('s', 's.png');
+// loadSprite('t', 't.png');
+// loadSprite('v', 'v.png');
+// loadSprite('z', 'z.png');
+
+// scene("game", () => {
+//     layers(['bg', 'obj', 'ui'], 'obj');
+
+//     const map = [
+//         '                                                                                   ',
+//         '                                                                                   ',
+//         '                                                                                   ',
+//         '                                                                                   ',
+//         '                                                                                   ',
+//         'w         j   j    j    j    j  m   mm                                             ',
+//         'w   j   j   j   jttt  j   ttt                                                      ',
+//         'w     j    sss   gffgffg                                                           ',
+//         'wbbbbeehqhbbbbbeeeeqheeebhebehqbhbebebebebbebebeeeee                               ',
+//         'nnnnnnnnnnnnnnnnzzzzzzzzzzzzzzzzzzzznnnnnnnnnnnnnnzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
+//     ];
+
+//     const levelCfg = {
+//         width: 40,
+//         height: 40,
+//         'q': [sprite('q'), scale(2), solid(), 'q', 'dangerous'],
+//         'h': [sprite('h'), scale(2), solid(), 'h', 'dangerous'],
+//         'z': [sprite('z'), scale(4), solid(1), 'z', 'dangerous'],
+//         'j': [sprite('j'), scale(.5), solid(1), 'j', 'dangerous'],
+//         'n': [sprite('n'), scale(4), solid(), 'n', 'dangerous'],
+//         's': [sprite('s'), scale(8), solid(), 's', 'dangerous'],
+//         't': [sprite('t'), scale(2), solid(), 't', 'dangerous'],
+//         'g': [sprite('g'), scale(6), solid(), 'g', 'dangerous'],
+//         'f': [sprite('f'), scale(2), solid(), 'f', 'dangerous'],
+//         'm': [sprite('m'), scale(2), solid(), 'm', 'dangerous'],
+//         'b': [sprite('b'), scale(2), solid(), 'a', 'dangerous'],
+//         'e': [sprite('e'), scale(3), solid(), 'e', 'dangerous'],
+//     };
+
+//     const gameLevel = addLevel(map, levelCfg);
+
+//     const player = add([
+//         sprite('v'),
+//         scale(.5),
+//         pos(20, 20)
+//     ]);
+
+//     player.collides('dangerous', () => {
+//         destroy(player);
+//     });
+
+//     player.action(() => {
+//         camPos(player.pos),
+//             camScale(.55);
+//     });
+
+//     const TIME_LEFT = 30;
+
+//     const timer = add([
+//         text('0'),
+//         pos(player.pos),
+//         scale(.5),
+//         layer('ui'),
+//         {
+//             time: TIME_LEFT,
+//         },
+//     ]);
+
+//     timer.action(() => {
+//         timer.time -= dt();
+//         timer.text = timer.time.toFixed(2);
+//         if (timer.time <= 0) {
+//             go('over, score.value');
+//         }
+
+//     });
+
+//     const score = add([
+//         text('0'),
+//         pos(5, 5),
+//         layer('ui'),
+//         {
+//             value: 0,
+//         }
+//     ]);
+
+//     const MOVE_SPEED = 100;
+
+//     keyDown('left', () => {
+//         player.move(-MOVE_SPEED, 0);
+//     });
+//     keyDown('right', () => {
+//         player.move(MOVE_SPEED, 0);
+//     });
+//     keyDown('up', () => {
+//         player.move(0, -MOVE_SPEED);
+//     });
+//     keyDown('down', () => {
+//         player.move(0, MOVE_SPEED);
+//     });
+
+//     function spawnBullet(p) {
+//         add([
+//             rect(5, 10),
+//             pos(p),
+//             origin('center'),
+//             color(.1, 0.9, 1),
+//             'bullet'
+//         ]);
+//     }
+
+//     const BULLET_SPEED = 100;
+//     action('bullet', (b) => {
+//         b.move(0, +BULLET_SPEED);
+//         if (b.pos.y < 0) {
+//             destroy(b);
+//         }
+//     });
 
 
-    keyPress('space', () => {
-        spawnBullet(player.pos.add(12, 30));
-    });
+//     keyPress('space', () => {
+//         spawnBullet(player.pos.add(12, 30));
+//     });
 
 
-    collides('bullet', 's', (b, s) => {
-        destroy(s);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 's', (b, s) => {
+//         destroy(s);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'a', (b, a) => {
-        destroy(a);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'a', (b, a) => {
+//         destroy(a);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'e', (b, e) => {
-        destroy(e);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'e', (b, e) => {
+//         destroy(e);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'f', (b, f) => {
-        destroy(f);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'f', (b, f) => {
+//         destroy(f);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'g', (b, g) => {
-        destroy(g);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'g', (b, g) => {
+//         destroy(g);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'h', (b, h) => {
-        destroy(h);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'h', (b, h) => {
+//         destroy(h);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'm', (b, m) => {
-        destroy(m);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'm', (b, m) => {
+//         destroy(m);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 't', (b, t) => {
-        destroy(t);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 't', (b, t) => {
+//         destroy(t);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    collides('bullet', 'v', (b, v) => {
-        destroy(v);
-        destroy(b);
-        score.value++;
-        score.text = score.value;
-    });
+//     collides('bullet', 'v', (b, v) => {
+//         destroy(v);
+//         destroy(b);
+//         score.value++;
+//         score.text = score.value;
+//     });
 
-    // if !state.user.name && !state.gameInProgress
-    // render form
-    // take in name
-    // onclick -> POST request to API
-    // if resolved, state.userLoggin
-    // state.user.name = response.username && state.gameInProgress = true
-    // else if state.username && state.gameInProgress
-    // render game
-    // when the game ends (kaboom event)
-    // POST to kaboom score to API (where user.id = state.user.id)
-    // if resolved -> render scoreboard
 
-    // if at anytime a request fails, you can render an error screen
-
-    if (!state.gameInProgress && !state.player.name && !state.player.id) {
-        //
-    }
-
-    if (state.gameInProgress && state.player.name && state.player.id) {
-        start("game");
-    };
-});
+// });
