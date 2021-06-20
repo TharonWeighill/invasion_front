8a368e665bde22e1a3f87b202c8f573aecfa1236
tharon <tharonweighill@gmail.com>
2021-06-19 19:16:27 -0600

    add player api class

--- javascripts/player.js
+++ javascripts/player.js
@@ -1,37 +1,19 @@
-class Session {
-    getPlayer() {
-        const body = document.getElementById('app');
-        body.innerHTML = `
-        <form id='player-form'>
-        <h1> Alien Invasion!!! </h1>
-        <label>Enter Name</label>
-        <input id='player-name'/>
-        <button type='submit'>START GAME</button
-        </form> 
-        `;
-        const form = document.getElementById('player-form');
-        form.addEventListener('submit', this.handleFromSubmit);
-    };
-    handleFromSubmit(e) {
-        e.preventDefault();
-        const input = document.getElementById('player-name');
-        return fetch("http://localhost:3000/players", {
-            method: "POST",
-            body: JSON.stringify({
-                name: input.value
-            }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then((resp) => resp.json())
-            .then((player) => console.log(player))
-            .catch((error) => console.error(error));
+class Player {
+    static all = [];
+
+    constructor({ name }) {
+        this.name = name;
+        Player.all.push(this);
     }
-    initGame(player) {
-        const body = document.getElementById('app');
-        body.innerHTML = "<canvas id='game'></canvas>";
-        const game = new Game(player);
-        game.start;
+
+    static findByName(name) {
+        return this.all.find(function (player) { player.name === name; });
     }
 }
+
+// initGame(player) {
+//     const body = document.getElementById('app');
+//     body.innerHTML = "<canvas id='game'></canvas>";
+//     const game = new Game(player);
+//     game.start;
+// }
--- javascripts/services/playerAPI.js
+++ javascripts/services/playerAPI.js
@@ -1,6 +1,70 @@
 class PlayerApi {
-    static fetchPlayers() {
-        fetch('http://localhost:3000/players')
+
+    static url = `${baseUrl}/players`;
+
+    static fetchPlayer() {
+        fetch(this.url)
+            .then(resp => resp.json())
+            .then(json => json.forEach(playObj => {
+                Player.findOrCreateBy(playObj);
+            }))
+            .catch(this.handleError);
+    }
+
+    static handleError(error) {
+        flash().innerText = error;
+        flash().classList.remove("hide");
+        setTimeout(() => {
+            flash().innerText = "";
+            flash().classList.add("hide");
+        }, 5000);
+    }
+
+    static handleSubmit(e) {
+        e.preventDefault();
+        const data = {
+            name: playerName().value,
+        };
+        fetch(playerApi.baseUrl, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
+            .then(json => {
+                let play = new Player(json);
+
+            });
+    }
+
+
+    getPlayer() {
+        const body = document.getElementById('app');
+        body.innerHTML = `
+        <form id='player-form'>
+        <h1> Alien Invasion!!! </h1>
+        <label>Enter Name</label>
+        <input id='player-name'/>
+        <button type='submit'>START GAME</button
+        </form> 
+        `;
+        const form = document.getElementById('player-form');
+        form.addEventListener('submit', this.handleFromSubmit);
+    };
+    handleFromSubmit(e) {
+        e.preventDefault();
+        const input = document.getElementById('player-name');
+        return fetch("http://localhost:3000/players", {
+            method: "POST",
+            body: JSON.stringify({
+                name: input.value
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
             .then((resp) => resp.json())
             .then((player) => console.log(player))
             .catch((error) => console.error(error));
